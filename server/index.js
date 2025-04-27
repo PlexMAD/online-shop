d9a5eda24311760fc8514bc426491306c428bbc8
PlexMAD <113678445+PlexMAD@users.noreply.github.com>
2025-04-27 03:16:29 +0300

    add create methods

--- server/index.js
+++ server/index.js
@@ -4,12 +4,17 @@ const express = require('express')
 const sequelize  =require('./db')
 const PORT = process.env.PORT || 7000
 const cors =  require('cors')
+const fileUpload = require('express-fileupload')
 const router = require('./routes/index')
 const app = express()
-
+const errorHandler = require('./middleware/ErrorHandlingMiddleware')
 app.use(cors())
 app.use(express.json())
+app.use(fileUpload({}))
 app.use('/api', router)
+
+
+app.use(errorHandler)
 
 const start = async () => {
     try {
@@ -22,3 +27,4 @@ const start = async () => {
 }
 
 start()
+
